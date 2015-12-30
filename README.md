523d88110588835bcbd13cb69dd064213608864c
Cordero <CCordero@cornerstone.com>
2015-12-30 13:13:08 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Flappy Bird
+====================
+
+This is my second graphical game using Python: A Flappy Bird Clone!
+
+Original author of Flappy Bird: Nguyễn Hà Đông
+This clone written by: Christopher Sabater Cordero
+
+Requirements: Python 2.7, Pygame 1.9.1
+
+How to run: python flappybird.py
---
+++ flappybird.py
@@ -0,0 +1,8 @@
+'''===============================================================================
+   Creator: Christopher Sabater Cordero
+   Original: Nguyễn Hà Đông
+   Date: 12/30/2015
+   File: Flappy Bird Clone
+   Notice: (c) Copyright 2015 by Christopher Sabater Cordero  All Rights Reserved.
+   ==============================================================================='''
+
