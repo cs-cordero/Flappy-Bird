972109c8751174b2a4e4dcdde5d34fcf703e9fa0
Christopher Sabater Cordero <chris.s.cordero@gmail.com>
2016-05-11 17:45:59 -0400

    Game is now playable. Added scoreboard, recurring pipes to dodge, and a way to restart the game when there is a Game Over.

--- flappybird.py
+++ flappybird.py
@@ -21,9 +21,10 @@ fps = pygame.time.Clock()
 pygame.display.set_caption('Flappy Bird')
 canvas = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
 canvas.set_colorkey(COLORKEY)
+artTime = random.randint(0,1)
 
 def generateBackground():
-  if random.randint(0,1) == 0: background_img = pygame.image.load(os.path.join('images','background_light.png')).convert()
+  if artTime == 0: background_img = pygame.image.load(os.path.join('images','background_light.png')).convert()
   else: background_img = pygame.image.load(os.path.join('images','background_dark.png')).convert()
   background_size = background_img.get_size()
   background_img = pygame.transform.scale(background_img,(int(WINDOWHEIGHT*1.1*(float(background_size[0])/float(background_size[1]))),WINDOWHEIGHT))
@@ -38,40 +39,94 @@ def loadPlayerSprites():
   for image in images: resized_images.append(pygame.transform.scale(image,(BIRDSIZE*(image.get_size()[0]/image.get_size()[1]),BIRDSIZE)))
   return resized_images
 
+def loadPipeSprites():
+  rects = []
+  for x in range(0,4): rects.append(pygame.Rect(26*x,0,26,160))
+  sheet = spritesheet(os.path.join('images','game_pipes.png'))
+  images = sheet.images_at(rects, COLORKEY)
+  pipeImgs = []
+  if artTime == 0:
+    pipeImgs.append(pygame.transform.scale(images[2],(PIPEWIDTH,WINDOWHEIGHT)))
+    pipeImgs.append(pygame.transform.scale(images[3],(PIPEWIDTH,WINDOWHEIGHT)))
+  else:
+    pipeImgs.append(pygame.transform.scale(images[0],(PIPEWIDTH,WINDOWHEIGHT)))
+    pipeImgs.append(pygame.transform.scale(images[1],(PIPEWIDTH,WINDOWHEIGHT)))
+  return pipeImgs
+
+def GenerateText(text, posx, posy, size=72, font='Flappy-Bird', color=WHITE):
+  fontObj = pygame.font.Font(os.path.join(os.path.dirname(__file__),'fonts',font + '.TTF'), size)
+  textSurfaceObj = fontObj.render(text, True, color)
+  textRectObj = textSurfaceObj.get_rect()
+  textRectObj.center = (posx, posy)
+  return textSurfaceObj, textRectObj
+
+def drawAssets(background_img, background_size, pipes, pipeImgs, player, flappybird, points):
+  gameZone = Rect(((WINDOWWIDTH/2)-(background_size[0]/2)),0,background_size[0],WINDOWHEIGHT)
+  canvas.fill(BLACK)
+  canvas.blit(background_img,gameZone.topleft)
+  if len(pipes) > 0:
+    for pipe in pipes:
+      canvas.blit(pipeImgs[0],(pipe.opening.left, pipe.opening.bottom))
+      canvas.blit(pipeImgs[1],(pipe.opening.left, pipe.opening.top - WINDOWHEIGHT))
+  pygame.draw.rect(canvas, BLACK, ((0,0),gameZone.bottomleft))
+  pygame.draw.rect(canvas, BLACK, (gameZone.topright,(WINDOWWIDTH,WINDOWHEIGHT)))
+  scoreBoard = GenerateText(str(points), gameZone.centerx, ((100.0/720.0)*WINDOWHEIGHT))
+  scoreBoardShadow = GenerateText(str(points), gameZone.centerx, ((100.0/720.0)*WINDOWHEIGHT), color=BLACK, size=96)
+  scoreBoardShadow[1].center = scoreBoard[1].center
+  canvas.blit(scoreBoardShadow[0], scoreBoardShadow[1])
+  canvas.blit(scoreBoard[0], scoreBoard[1])
+  canvas.blit(player,(flappybird.posx,flappybird.posy))
+
 def main():
-  background_img, background_size = generateBackground()
-  flappybird = bird(loadPlayerSprites(), frames=FPS/12)
-  player = flappybird.image_list[0]
-
   while True:
-    canvas.fill(BLACK)
-    canvas.blit(background_img,((WINDOWWIDTH/2)-(background_size[0]/2),0))
-    
-    try:
-      canvas.blit(flappybird.image_list[0],(10,10))   
-      canvas.blit(flappybird.image_list[1],(10,40))
-      canvas.blit(flappybird.image_list[2],(10,70))
-      canvas.blit(flappybird.image_list[3],(10,100))
-      canvas.blit(flappybird.image_list[4],(10,130))
-      canvas.blit(flappybird.image_list[5],(10,160))
-      canvas.blit(flappybird.image_list[6],(10,190))
-      canvas.blit(flappybird.image_list[7],(10,220))
-      canvas.blit(flappybird.image_list[8],(10,250))
-    except: pass
+    GameOver = False
+    ResetGame = False
+    elapsed_time = 0
+    points = 0
+    background_img, background_size = generateBackground()
+    flappybird = bird(loadPlayerSprites(), frames=FPS/12)
+    player = flappybird.image_list[0]
+    pipeImgs = loadPipeSprites()
+    pipes = []
 
-    canvas.blit(player,(flappybird.posx,flappybird.posy))
+    while ResetGame == False:
+      drawAssets(background_img, background_size, pipes, pipeImgs, player, flappybird, points)
 
-    for event in pygame.event.get():
-      if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-        flappybird.rotate(flap=True)
-        flappybird.fall(flap=True)
-      if event.type == QUIT:
-        pygame.quit()
-        sys.exit()
-    
-    pygame.display.update()
-    fps.tick(60)
-    player = flappybird.next()
+      for event in pygame.event.get():
+        if (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1) or (event.type == pygame.KEYDOWN and event.key == K_SPACE):
+          if GameOver == False:
+            flappybird.rotate(flap=True)
+            flappybird.fall(flap=True)
+          elif GameOver == True and flappybird.posy == (WINDOWHEIGHT - BIRDSIZE):
+            ResetGame = True
+        if event.type == QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_F4 and bool(event.mod & pygame.KMOD_ALT)) or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+          pygame.quit()
+          sys.exit()
+        
+      if GameOver == False:
+        for pipe in pipes:
+          pipe.next()
+          checkDeath, points = pipe.checkCollision(flappybird, points)
+          if checkDeath == "Killed": GameOver = True
+          if pipe.opening.right < (WINDOWWIDTH/2)-(background_size[0]/2):
+            pipes.remove(pipe)
+        elapsed_time += fps.get_time()
+        if elapsed_time > 2000:
+          elapsed_time = 0
+          pipes.append(gamePipe(pipeImgs, background_size))
+
+      player = flappybird.next()
+      # check for bottom collision
+      if (flappybird.posy + BIRDSIZE) >= WINDOWHEIGHT:
+        if flappybird.loop == True:
+          sounds.channel2.play(sounds.sound_hit)
+          sounds.channel2.queue(sounds.sound_die)
+        flappybird.posy = (WINDOWHEIGHT - BIRDSIZE)
+        flappybird.loop = False
+        GameOver = True
+      
+      pygame.display.update()
+      fps.tick(60)
 
 if __name__ == '__main__':
   main()
--- scripts/classes.py
+++ scripts/classes.py
@@ -12,14 +12,16 @@ class bird(object):
     self.image_list.append(bird_imgs[2+(x*3)])
 
     # positional variables
-    self.posx = 60.0
+    self.posx = (WINDOWWIDTH / 2) - (BIRDSIZE / 2)
     self.posy = 60.0
     self.dy = 0.00
+    self.right = self.posx + BIRDSIZE
+    self.center = self.posx + (BIRDSIZE/2)
 
     # animation variables
     self.i = 0
     self.f = frames
-    self.loop = loop
+    self.loop = True
     self.frames = frames
     
     # angle variables
@@ -27,12 +29,12 @@ class bird(object):
     self.dt_angle = -0.1
 
   def next(self):
-    if self.i >=(len(self.image_list)):
-      if self.loop: self.i = 0
-      else: pass
-    self.fall()
-    self.rotate()
-    image = pygame.transform.rotate(self.image_list[self.i],self.angle)
+    if self.i >=(len(self.image_list)): self.i = 0
+    self.fall()  # manipulate y position
+    self.rotate()  # manipulate the rotation of the bird
+    image = pygame.transform.rotate(self.image_list[self.i],self.angle)  # manipulate the rotation of the bird
+
+    # loops the sprite list to animate the bird flapping wings
     if self.loop:
       self.f -= 1
       if self.f == 0:
@@ -54,15 +56,33 @@ class bird(object):
     if flap == False:
       self.posy += self.dy
       self.dy += FALLSPEED
-      if (self.posy + BIRDSIZE) >= WINDOWHEIGHT:
-        if self.loop == True:
-          sounds.channel2.play(sounds.sound_hit)
-          sounds.channel2.queue(sounds.sound_die)
-        self.posy = (WINDOWHEIGHT - BIRDSIZE)
-        self.loop = False
     elif flap == True:
       self.posy -= FLAPHEIGHT
       self.dy = 0.00
       self.loop = True
       sounds.channel1.play(sounds.sound_wingflap)
       if self.posy <= 0: self.posy = 0
+
+
+class gamePipe(object):
+  def __init__(self, pipeImgs, gameZone):
+    self.status = "Incoming"
+    self.opening = pygame.Rect((WINDOWWIDTH/2)+(gameZone[0]/2), random.randint(30, WINDOWHEIGHT-PIPEOPENING-60),PIPEWIDTH, PIPEOPENING)
+    self.images = pipeImgs
+
+  def next(self):
+    self.opening.left -= PIPESPEED
+
+  def checkCollision(self, player, points):
+    # player is horizontally between the pipes
+    if player.right > self.opening.left and player.right < self.opening.right:
+      if self.status != "Killed" and (player.posy < self.opening.top or player.posy + BIRDSIZE > self.opening.bottom):
+        self.status = "Killed"
+        player.loop = False
+        sounds.channel2.play(sounds.sound_hit)
+        sounds.channel2.queue(sounds.sound_die)
+      elif self.status == "Incoming" and player.center >= self.opening.centerx:
+        self.status = "Passed"
+        points += 1
+        sounds.channel2.play(sounds.sound_point)
+    return self.status, points
